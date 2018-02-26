b574e5cffdc907650a99690a85e4981c65367711
adamo.meirelles <adamo.meirelles@sankhya.com.br>
2018-02-25 21:24:27 -0300

    Versão Inicial exercicio

---
+++ projeto/usuario_cadastrar.php
@@ -0,0 +1,25 @@
+<?php
+
+	//var_dump($_POST);
+	//exit;
+
+	include "../includes/conexao.php";
+
+	$nome = $_POST["nome"];
+	$email = $_POST["email"];
+	$assuntos = $_POST["assuntos"];
+	$assuntos_string = implode(", ", $assuntos);
+
+	$sql = "INSERT INTO usuario (nome, email, assuntos) 
+		VALUES ('$nome', '$email', '$assuntos_string')
+	";
+
+
+	$query = mysqli_query($conexao, $sql);
+
+	if($query){
+		header("location: usuario.php?mensagem=sucesso");
+	}else{
+		header("location: usuario.php?mensagem=erro");
+	}
+
